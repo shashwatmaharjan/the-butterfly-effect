3852357c50548d68e31177853fe0eb9e7cdcf6e7
shashwatmaharjan <shashwatmjn@gmail.com>
2024-11-21 23:59:49 -0500

    Added

---
+++ scripts/main.py
@@ -0,0 +1,15 @@
+# Import necessary libraries
+import os
+
+# Main function
+def main():
+    
+    pass
+
+if __name__ == '__main__':
+    
+    # Clear the console regardless of the OS
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+    # Call the main function
+    main()
