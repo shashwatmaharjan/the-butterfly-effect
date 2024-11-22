471fa25386813015f3cfa6ee7391e9a4be6a079e
shashwatmaharjan <shashwatmjn@gmail.com>
2024-11-22 00:33:44 -0500

    Defined lorenz function

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,31 @@
 # Import necessary libraries
 import os
+import sympy as sp
+
+# Lorenz ODE
+def lorenz_ode(sigma, rho, beta):
+    
+    # Define the independent variable
+    t = sp.symbols('t')
+    
+    # Define the dependent variables
+    x = sp.Function('x')(t)
+    y = sp.Function('y')(t)
+    z = sp.Function('z')(t)
+    
+    # Define derivatives for the Lorenz ODE
+    x_dot = sigma*(y - x)
+    y_dot = x*(rho - z) - y
+    z_dot = x*y - beta*z
+    
+    return [x_dot, y_dot, z_dot]
+    
 
 # Main function
 def main():
     
     pass
+    
 
 if __name__ == '__main__':
     
