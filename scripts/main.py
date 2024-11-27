a42baccc27ba960539735a8073cfc737c6a7e2b9
shashwatmaharjan <shashwatmjn@gmail.com>
2024-11-27 12:30:10 -0500

    Updated the text alignment

--- scripts/main.py
+++ scripts/main.py
@@ -78,9 +78,9 @@ def main():
                         children='Initial Condition 1',
                         style={'textAlign': 'center'}),
                         
-                html.Div([html.Div(['x: ', dcc.Input(id='x0_1', type = 'number', value = 0, style={'width': '40px'}, min=-10, max=10, required=True),]),
-                        html.Div(['y: ', dcc.Input(id='y0_1', type = 'number', value = 1, style={'width': '40px'}, min=-10, max=10, required=True),]),
-                        html.Div(['z: ', dcc.Input(id='z0_1', type = 'number', value = 0, style={'width': '40px'}, min=-10, max=10, required=True),]),],
+                html.Div([html.Div(['x: ', dcc.Input(id='x0_1', type = 'number', value = 0, style={'width': '40px', 'text-align': 'center'}, min=-10, max=10, required=True),]),
+                        html.Div(['y: ', dcc.Input(id='y0_1', type = 'number', value = 1, style={'width': '40px', 'text-align': 'center'}, min=-10, max=10, required=True),]),
+                        html.Div(['z: ', dcc.Input(id='z0_1', type = 'number', value = 0, style={'width': '40px', 'text-align': 'center'}, min=-10, max=10, required=True),]),],
                         
                         style={'display': 'flex',
                                 'justify-content': 'center',
@@ -96,9 +96,9 @@ def main():
                         children='Initial Condition 2',
                         style={'textAlign': 'center'}),
                         
-                html.Div([html.Div(['x: ', dcc.Input(id='x0_2', type = 'number', value = 0, style={'width': '40px'}, min=-10, max=10, required=True),]),
-                        html.Div(['y: ', dcc.Input(id='y0_2', type = 'number', value = 1, style={'width': '40px'}, min=-10, max=10, required=True),]),
-                        html.Div(['z: ', dcc.Input(id='z0_2', type = 'number', value = 0, style={'width': '40px'}, min=-10, max=10, required=True),]),],
+                html.Div([html.Div(['x: ', dcc.Input(id='x0_2', type = 'number', value = 0, style={'width': '40px', 'text-align': 'center'}, min=-10, max=10, required=True),]),
+                        html.Div(['y: ', dcc.Input(id='y0_2', type = 'number', value = 1, style={'width': '40px', 'text-align': 'center'}, min=-10, max=10, required=True),]),
+                        html.Div(['z: ', dcc.Input(id='z0_2', type = 'number', value = 0, style={'width': '40px', 'text-align': 'center'}, min=-10, max=10, required=True),]),],
                         
                         style={'display': 'flex',
                                 'justify-content': 'center',
