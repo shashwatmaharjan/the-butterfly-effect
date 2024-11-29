51e925f21ae7a0de17f009f8f4630f4a0061cb7b
shashwatmaharjan <shashwatmjn@gmail.com>
2024-11-29 10:38:51 -0500

    Centered the animate button

--- scripts/main.py
+++ scripts/main.py
@@ -101,7 +101,7 @@ def plot_time_versus_xyz(solution_1, solution_2, timepoints, color_1, color_2, b
             x=0.5,
             font=dict(size=font_size)),
         font=font_style,
-        updatemenus=[dict(type='buttons', buttons=[dict(label='Animate',
+        updatemenus=[dict(type='buttons', x=0.5, xanchor='center', y=2, buttons=[dict(label='Animate',
                                                          method='animate',
                                                          args=[None, {"frame": {"duration": 100, "redraw": True}, "fromcurrent": True, "transition": {"duration": 0}}])])],
     )
